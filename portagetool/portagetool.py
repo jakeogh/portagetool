5b7670d8d21edf0fb506926aaf52ba4628f3dbcc
jakeogh <github.com@v6y.net>
2022-01-10 23:52:22 -0700

    auto-commit

--- portagetool/portagetool.py
+++ portagetool/portagetool.py
@@ -229,6 +229,7 @@ def files_provided_by_package(ctx,
     qlist_stdout_lines = qlist_command.stdout.splitlines()
     ic(qlist_stdout_lines)
     for line in qlist_stdout_lines:
+        ic(line)
         output(line, tty=tty, verbose=verbose)
 
 
