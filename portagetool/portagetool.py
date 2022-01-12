d7bfb65c1643e609e9ea51cde4c04654903fea00
jakeogh <github.com@v6y.net>
2022-01-11 17:39:51 -0700

    auto-commit

--- portagetool/portagetool.py
+++ portagetool/portagetool.py
@@ -237,7 +237,8 @@ def generate_patched_package_source(ctx,
         raise FileNotFoundError(package_location_command_stdout)
     ic(package_location)
 
-    ebuild_path = Path(os.fsdecode(package_location)) / Path(Path(os.fsdecode(package + b'.ebuild').name))
+    package_name_and_version = package.split(b'/')[-1]
+    ebuild_path = Path(os.fsdecode(package_location)) / Path(os.fsdecode(package_name_and_version + b'.ebuild'))
     ic(ebuild_path)
     ebuild_clean_command = sh.ebuild(ebuild_path, 'clean', **sh_oet)
     ebuild_unpack_command = sh.ebuild(ebuild_path, 'unpack', **sh_oet)
